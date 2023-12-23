c67e4c5a7eb2fd834b3c992d6dffa31293a363b2
Matiss Cukurs <cukursmatiss@gmail.com>
2023-12-23 10:09:59 +0200

    finish 228

--- src/com/chukurs/MainMailer.java
+++ src/com/chukurs/MainMailer.java
@@ -9,10 +9,10 @@ public class MainMailer {
     public static void main(String[] args) {
         //List with duplicate 'names'
         //use Map to keep track of 'Accounts' for each distinct 'name'
-        String[] names = {"Anna Jones", "Ann Jones Ph.D.",
+        String[] names = {"Anna Jones", "Anna Jones Ph.D.",
                 "Bob Jones M.D.",
                 "Carol Jones",
-                "Ed Greend Ph.D.", "Ed Green M.D.",
+                "Ed Green Ph.D.", "Ed Green M.D.",
                 "Ed Black"};
 
         List<StringBuilder> population = getNames(names);
@@ -22,11 +22,22 @@ public class MainMailer {
             //i use this on purpose, to strengthen the understanding of using interfaces
         });
         System.out.println(counts);
-        StringBuilder annJonesPhd = new StringBuilder("Ann Jones Ph.D.");
+        StringBuilder annJonesPhd = new StringBuilder("Anna Jones Ph.D.");
         System.out.println("There are " + counts.get(annJonesPhd) + " records for " + annJonesPhd);
 
         List<StringBuilder> cleanedNames = standardizeNames(population);
         System.out.println(cleanedNames);
+
+        System.out.println("There are " + counts.get(annJonesPhd) + " records for " + annJonesPhd);
+        System.out.println(counts);
+        StringBuilder annJones = new StringBuilder("Anna Jones");
+        System.out.println("There are " + counts.get(annJones) + " records for " + annJones);
+        System.out.println("-".repeat(20));
+        counts.forEach((k,v)-> System.out.println(k+":"+v));
+        System.out.println("-".repeat(20));
+        counts.keySet().forEach(k-> System.out.println(k+ ":" +counts.get(k)));
+
+
 
     }
 
